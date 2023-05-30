648f2029b81c2d82ebf4fd6a53274403fd1dd5f0
Slim <slim@pirate.tn>
2023-05-30 15:51:57 +0100

    fixed: php-cs-fixer

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -86,7 +86,6 @@ use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Mercure\HubInterface;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services()
